5d8fcf5264b1a8225dbd2b1652e7c2d986f48db8
MatheusCordeiro52 <matheuscamilo090@gmail.com>
2025-02-26 18:54:23 +0000

    COMMIT NIVEL MESTRE

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,37 +1,100 @@
-#include<stdio.h>
-int main(){
-    char linhas[10] ={'A' ,'B' ,'C' ,'D' , 'E' ,'F' , 'G' ,'H' , 'I' ,'J'};
-    int tabuleiro[10][10] = {0};
+#include <stdio.h>
+#define TAM 10
+#define TAM_HAB 5
 
-    tabuleiro[3][3] = 3;
-    tabuleiro[3][4] = 3;
-    tabuleiro[3][5] = 3;
-
-    tabuleiro[3][7] = 3;
-    tabuleiro[4][7] = 3;
-    tabuleiro[5][7] = 3;
-
-    tabuleiro[3][1] = 3;
-    tabuleiro[4][2] = 3;
-    tabuleiro[5][3] = 3;
-
-    tabuleiro[7][7] = 3;
-    tabuleiro[8][8] = 3;
-    tabuleiro[9][9] = 3;
-
-
-    printf("TABULEIRO BATALHA NAVAL\n");
-    printf("  ");
-    for (int i = 0; i < 10; i++){
-        printf("%c ", linhas[i]);
+// Função para inicializar o tabuleiro com água (0)
+void inicializarTabuleiro(int tabuleiro[TAM][TAM]) {
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            tabuleiro[i][j] = 0;
+        }
     }
-    for (int e = 0; e < 10; e++){
-        printf("\n%2d ", e+1);
-    for (int f = 0; f < 10; f++){
-        printf("%d ", tabuleiro[e][f]);
-    }
-    
-    
 }
-    return 0;   
+
+// Função para posicionar navios no tabuleiro
+void posicionarNavios(int tabuleiro[TAM][TAM]) {
+    // Navio vertical
+    for (int i = 2; i < 5; i++) {
+        tabuleiro[i][4] = 3; // Navio na coluna 4 (E)
+    }
+    // Navio horizontal
+    for (int j = 1; j < 4; j++) {
+        tabuleiro[6][j] = 3; // Navio na linha 6 (7)
+    }
+    // Navios na diagonal
+    for (int d = 0; d < 3; d++) {
+        tabuleiro[d][d] = 3; // Diagonal principal
+        tabuleiro[d][9 - d] = 3; // Diagonal inversa
+    }
+}
+
+// Função para exibir o tabuleiro
+void exibirTabuleiro(int tabuleiro[TAM][TAM]) {
+    printf("   A B C D E F G H I J\n"); // Cabeçalho com letras das colunas
+    for (int i = 0; i < TAM; i++) {
+        printf("%2d ", i + 1); // Numeração das linhas (1 a 10)
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ", tabuleiro[i][j]); // Imprime os valores da matriz
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// Função para sobrepor uma matriz de habilidade no tabuleiro
+void aplicarHabilidade(int tabuleiro[TAM][TAM], int habilidade[TAM_HAB][TAM_HAB], int x, int y, int tipo) {
+    for (int i = 0; i < TAM_HAB; i++) {
+        for (int j = 0; j < TAM_HAB; j++) {
+            int posX = x + i - TAM_HAB / 2;
+            int posY = y + j - TAM_HAB / 2;
+            if (posX >= 0 && posX < TAM && posY >= 0 && posY < TAM) {
+                if (habilidade[i][j] == 1) {
+                    tabuleiro[posX][posY] = tipo; // Usar o tipo para representar diferentes habilidades
+                }
+            }
+        }
+    }
+}
+
+// Matrizes de habilidades
+int cone[TAM_HAB][TAM_HAB] = {
+    {0, 0, 1, 0, 0},
+    {0, 1, 1, 1, 0},
+    {1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0}
+};
+
+int cruz[TAM_HAB][TAM_HAB] = {
+    {0, 0, 1, 0, 0},
+    {0, 0, 1, 0, 0},
+    {1, 1, 1, 1, 1},
+    {0, 0, 1, 0, 0},
+    {0, 0, 1, 0, 0}
+};
+
+int octaedro[TAM_HAB][TAM_HAB] = {
+    {0, 0, 1, 0, 0},
+    {0, 1, 1, 1, 0},
+    {1, 1, 1, 1, 1},
+    {0, 1, 1, 1, 0},
+    {0, 0, 1, 0, 0}
+};
+
+int main() {
+    int tabuleiro[TAM][TAM];
+    inicializarTabuleiro(tabuleiro);
+    
+    // Posicionar navios no tabuleiro
+    posicionarNavios(tabuleiro);
+    
+    // Aplicar todas as habilidades no mesmo tabuleiro
+    aplicarHabilidade(tabuleiro, cone, 2, 2, 4);      // Habilidade cone
+    aplicarHabilidade(tabuleiro, cruz, 4, 4, 5);      // Habilidade cruz
+    aplicarHabilidade(tabuleiro, octaedro, 6, 6, 6);  // Habilidade octaedro
+    
+    printf("TABULEIRO BATALHA NAVAL\n");
+    exibirTabuleiro(tabuleiro);
+    
+    return 0;
 }
